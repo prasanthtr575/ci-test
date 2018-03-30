6b67330e8eb00ffaa2ed488c6b8e0d59f9c7d2c4
prasanthtr575 <mailprasanthtr@gmail.com>
2018-03-30 08:30:57 +0530

    travis ci added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Continuous Intgeration';
 }
+
+/* 1/CWnQlk3XAmJhEuLHup_qqUaCUdFPK95ifZNWITlZKFRLi3KZKFOlyiqU9-lRwLZL */
+//Example: firebase deploy --token "$FIREBASE_TOKEN"
